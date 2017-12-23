c2bc22dd156338e542cb112136f641462174b521
katiesteckles <katiesteckles@gmail.com>
2017-12-23 19:09:23 +0000

    Update mggtree.py

--- mggtree.py
+++ mggtree.py
@@ -9,8 +9,8 @@ import colorsys
 blinkt.clear()
 blinkt.show()
 
-APP_KEY = 'JIc3EBbNiztKN0JXmVtvX75jG'
-APP_SECRET = '4lE1SQefBkWMIjzChTFh1BBc13vEGIGGKOC8ciuOaVW4p9yMPx'
+APP_KEY = ''
+APP_SECRET = ''
 twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
 ACCESS_TOKEN = twitter.obtain_access_token()
 twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
